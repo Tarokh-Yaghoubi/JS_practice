b00efd40b43fcfa5bed3c3c12b951587cba831fb
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-03-26 22:55:59 +0330

    a simple math module, using it in server.js to show how we use modules in NodeJS

---
+++ 0010_learning_nodejs/0000_start_here/math.js
@@ -0,0 +1,7 @@
+// we can even do this, instead of using module.exports = { obj }
+exports.add = (a, b) => a + b
+exports.subtract = (a, b) => a - b
+exports.multiply = (a, b) => a * b
+exports.divide = (a, b) => a / b
+
+// module.exports = { add, subtract, multiply, divide }
