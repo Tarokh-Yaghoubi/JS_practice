d699760ae4cac1f63e29408240f131f75032ec5f
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-03-31 22:44:21 +0330

    useful math methods in JavaScript | learning JS

---
+++ 0011_learning_javascript/00011_math/server.js
@@ -0,0 +1,23 @@
+// Math Methods and Properties
+
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))    // removes all the decimals
+
+console.log(Math.round(Math.PI))    // returns 3
+console.log(Math.ceil(Math.PI))     // this will round up no matter what is the decimal
+console.log(Math.floor(Math.PI))    // this will round down no matter what is the decimal 
+
+
+console.log(Math.pow(2, 2))          // this is 2 to the power of 2
+
+// MIN and MAX
+
+console.log(Math.min(3, 2, 4))
+console.log(Math.max(4, 5, 3))
+
+console.log(Math.random())      // this will always generate a random number from 0 to 1, but it never includes the 1 itself.
+
+// A JavaScript expression that generates a random number from 1 to 10
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
