8d39a8ba122eaa50fda36f80dc60179045e04595
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-03-31 12:44:40 +0330

    index.js | used uuid and date-fns packages for tests | learning nodejs

--- 0010_learning_nodejs/0010_npm_modules/index.js
+++ 0010_learning_nodejs/0010_npm_modules/index.js
@@ -1,2 +1,9 @@
 
-console.log("testing")
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))   // \t is the tab delimited
+console.log(uuid()) // this will generate a different ID each time
+
+
