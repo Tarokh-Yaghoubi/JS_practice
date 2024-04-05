5695dcf687d8077f9691ba2b7bb0d04d0340c514
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-04-05 23:08:39 +0330

    method overriding in JS | learning JS

--- 0011_learning_javascript/00111_classes/classes_inheritance.js
+++ 0011_learning_javascript/00111_classes/classes_inheritance.js
@@ -43,3 +43,87 @@ let rabbit = new Rabbit("Rabbit")
 rabbit.hide()
 rabbit.run(50)
 rabbit.stop()
+
+// Object of Rabbit class have access to both Rabbit methods such as hide(). and Animal methods such as run().
+// Internally, "extends" keyword works using the good old prototype mechanics. It sets Rabbit.prototype.
+// [[ Prototype ]] to Animal.prototype. So if a method is not found in Rabbit.prototype, JavaScript takes it
+// from Animal.prototype.
+
+// Class syntax allows you to specify not just a class, but any expressions after extends.
+
+function f(phrase) {
+    return class {
+        echo() {
+            console.log(phrase)
+        }
+    }
+}
+
+class User extends f("If you do not know the answer, the answer is no") {}
+
+new User().echo()
+
+// Here, class User, inherits from the result of f("something").
+
+
+// OVERRIDING A METHOD
+
+// By default, all methods that are not specified in class Rabbit are taken directly "as they are" from class Animal.
+
+// But if we specify our own method in Rabbit, such as stop(), then it will be used instead
+// an example with tarokh and people. 
+
+class People {
+    constructor(firstname, lastname) {
+        this._firstname = firstname;
+        this._lastname = lastname;
+    }
+
+    set firstname(value) {
+        this._firstname = value
+    }
+
+    get firstname() {
+        return this._firstname
+    }
+
+    set lastname(value) {
+        this._lastname = value
+    }
+
+    get lastname() {
+        return this._lastname
+    }
+
+    walk() {
+        console.log(`user ${this._firstname} is walking`)
+    }
+
+    stop() {
+        console.log(`user ${this._firstname} stopped walking`)
+    }
+}
+
+// Usually, however, we do not want to totally replace a parent method, but rather to build on top of it to tweak or extend its functionality.
+// we do something in our method, but call the parent method before/after it or in the process.
+
+// Classes provide "super" keyword for that
+// super.method(...) to call a parent method .
+// super(...) to call a parent constructor (inside our constructor only)
+
+class Tarokh extends People {
+
+    sit() {
+        console.log(`user ${this._firstname} sat on the floor`)
+    }
+
+    // calling stop of People, and adding more functionality.
+    stop() {
+        super.stop()
+        this.sit()
+    }
+}
+
+let me = new Tarokh("Tarokh", "Yaghoubi");
+me.stop();
+
